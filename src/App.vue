cd64fcf664f47416d34bb46a0e5cd6c595255771
Luna-Lovegoood <fnzhouwei@163.com>
2021-10-29 11:48:18 +0800

    feat: 接入less、p5

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,35 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VueP5 @setup="setup"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import VueP5 from 'vue-p5'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    VueP5,
+  },
+  methods: {
+    setup(sk) {
+      sk.createCanvas(400, 400)
+      sk.background(255, 0, 0)
+    }
   }
 }
 </script>
 
-<style>
+<style lang="less">
 #app {
+  img {
+    width: 500px;
+    height: 500px;
+  }
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
